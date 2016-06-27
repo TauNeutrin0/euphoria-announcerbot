d525e683ded012628496809fb3567435ca9a6ad9
TauNeutrin0 <tauneutrin00@gmail.com>
2016-06-27 22:11:06 +0000

    Add status command, make bot count replies to itself

--- src/main/java/AnnouncerBot.java
+++ src/main/java/AnnouncerBot.java
@@ -5,13 +5,18 @@ import java.util.regex.Pattern;
 
 import euphoria.Bot;
 import euphoria.FileIO;
+import euphoria.RoomNotConnectedException;
 import euphoria.events.ConnectMessageEventListener;
 import euphoria.events.ConsoleEventListener;
 import euphoria.events.MessageEvent;
 import euphoria.events.MessageEventListener;
+import euphoria.events.PacketEvent;
 import euphoria.events.PacketEventListener;
+import euphoria.events.ReplyEventListener;
 import euphoria.events.StandardEventListener;
+import euphoria.packets.commands.Send;
 import euphoria.packets.events.SendEvent;
+import euphoria.packets.replies.SendReply;
 
 
 public class AnnouncerBot extends Bot {
@@ -57,6 +62,9 @@ public class AnnouncerBot extends Bot {
                         room = m.group(1);
                         System.out.println("Set room to " + m.group(1) + ".");
                     }
+                } else if (command.matches("^!status$")) {
+                    System.out.println("\n"+(map.size()-1)+"/"+msgLimit+" messages in &"+room+".");
+                    System.out.println("Announcement: \n"+announcement+"\n");
                 }
             }
         });
@@ -74,7 +82,7 @@ public class AnnouncerBot extends Bot {
                             e.printStackTrace();
                         }
 
-                        if (map.size() >= msgLimit) {
+                        if (map.size()-1 >= msgLimit) {
                             evt.getRoomConnection().sendMessage(announcement);
                             map.clear();
                         }
@@ -83,8 +91,8 @@ public class AnnouncerBot extends Bot {
                             if (map.get(parentId) < 5) {
                                 map.put(evt.getId(), 0);
                                 map.replace(parentId, map.get(parentId) + 1);
-                                if (map.size() >= msgLimit) {
-                                    evt.getRoomConnection().sendMessage(announcement);
+                                if (map.size()-1 >= msgLimit) {
+                                    announce();
                                     map.clear();
                                 }
                             }
@@ -96,7 +104,27 @@ public class AnnouncerBot extends Bot {
 
         System.out.println("\n#####\nUse \"!announcement [message]\" to change the announcement.\n"
                            + "Use \"!messages [number]\" to change the number of messages after which to trigger.\n"
-                           + "Make sure you set the room using \"!room &[room]\"\n#####\n");
+                           + "Make sure you set the room using \"!room &[room]\"\n"
+                           + "\"!status gives the current state of the bot.\"\n#####\n");
+    }
+    
+    public void announce() {
+        try {
+            getRoomConnection(room).sendPacket(new Send(announcement).createPacket(), new ReplyEventListener() {
+                @Override
+                public void onReplySuccess(PacketEvent evt) {
+                    map.put(((SendReply)evt.getPacket().getData()).getId(), 0);
+                    System.out.println("Announced in "+room+".");
+                }
+                @Override
+                public void onReplyFail(PacketEvent evt) {
+                    System.out.println("Message error - could not announce in "+room+".\nEuphoria returned error "+evt.getPacket().getError()+".");
+                }
+                public void onReplyEvent(PacketEvent evt) {}
+            });
+        } catch (RoomNotConnectedException e) {
+            System.out.println("Connection error - not connected to "+room+".");
+        }
     }
 
     public static void main(String[] args) {
